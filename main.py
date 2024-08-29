0ca99798c3ac8a5103d3209ae9688ac5eddf0c0d
HrushikeshKoppula <95178027+HrushikeshKoppula@users.noreply.github.com>
2024-08-29 17:17:09 +0530

    onestep bs and draw done

--- main.py
+++ main.py
@@ -14,56 +14,88 @@ target_color = "green"
 text_color = pygame.Color("#EEEEEE")
 font = pygame.font.Font("FiraCode-Regular.ttf", 24)
 
-n = int(input("n= "))
-numbers = []
-print("Enter n numbers:")
-for i in range(n):
-    num = int(input())
-    numbers.append(num)
-target = int(input("target= "))
-numbers.sort()
-
-
-
-def draw_boxes(numbers, low, high, mid, target_index=None):
-    box_width = 50
-    box_height = 25
-    margin = 10
-    x_start = (screen_width-(box_width+margin)*n)//2
-    y_start = screen_height//2-box_height//2
-    for i, number in enumerate(numbers):
-        x = x_start+i*(box_width + margin)
-        y = y_start
-        if i == target_index:
-            color = target_color
-        elif i == low or i == high:
-            color = low_high_color
-        elif i == mid:
-            color = mid_color
+class Visualizer:
+    def __init__(self):
+        self.n = int(input("n= "))
+        self.numbers = []
+        print("Enter n numbers:")
+        for i in range(self.n):
+            num = int(input())
+            self.numbers.append(num)
+        self.target = int(input("target= "))
+        self.numbers.sort()
+        self.low = 0
+        self.high = self.n-1
+        self.mid = 0
+    
+    def Draw(self):
+        box_width = 50
+        box_height = 25
+        margin = 10
+        x_start = (screen_width-(box_width+margin)*self.n)//2
+        y_start = screen_height//2-box_height//2
+        for i, number in self.n:
+            x = x_start+i*(box_width + margin)
+            y = y_start
+            if i == self.low or i == self.high:
+                color = low_high_color
+            elif i == self.mid:
+                color = mid_color
+            else:
+                color = box_color
+            pygame.draw.rect(screen, color, (x, y, box_width, box_height))
+            text_surface = font.render(str(number), True, text_color)
+            text_rect = text_surface.get_rect(center=(x+box_width//2,y+box_height//2))
+            screen.blit(text_surface, text_rect)
+    
+    def OneStepBinarySearch(self):
+        self.mid = low+(high-low)//2
+        if self.numbers[self.mid] == self.target:
+            target_index = self.mid
+        elif self.numbers[self.mid] < self.target:
+            low = self.mid+1
         else:
-            color = box_color
-        pygame.draw.rect(screen, color, (x, y, box_width, box_height))
-        text_surface = font.render(str(number), True, text_color)
-        text_rect = text_surface.get_rect(center=(x+box_width//2,y+box_height//2))
-        screen.blit(text_surface, text_rect)
+            high = self.mid-1
 
-def binary_search(numbers, target):
-    low = 0
-    high = len(numbers)-1
-    while low <= high:
-        mid = (low + high) // 2
-        draw_boxes(numbers, low, high, mid, target_index)
-        pygame.display.flip()
-        if numbers[mid] == target:
-            target_index = mid
-            break
-        elif numbers[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-    draw_boxes(numbers, low, high, mid, target_index)
-    pygame.display.flip()
-    return target_index
+# def draw_boxes(numbers, low, high, mid, target_index=None):
+#     box_width = 50
+#     box_height = 25
+#     margin = 10
+#     x_start = (screen_width-(box_width+margin)*n)//2
+#     y_start = screen_height//2-box_height//2
+#     for i, number in enumerate(numbers):
+#         x = x_start+i*(box_width + margin)
+#         y = y_start
+#         if i == target_index:
+#             color = target_color
+#         elif i == low or i == high:
+#             color = low_high_color
+#         elif i == mid:
+#             color = mid_color
+#         else:
+#             color = box_color
+#         pygame.draw.rect(screen, color, (x, y, box_width, box_height))
+#         text_surface = font.render(str(number), True, text_color)
+#         text_rect = text_surface.get_rect(center=(x+box_width//2,y+box_height//2))
+#         screen.blit(text_surface, text_rect)
+
+# def binary_search(numbers, target):
+#     low = 0
+#     high = len(numbers)-1
+#     while low <= high:
+#         mid = (low + high) // 2
+#         draw_boxes(numbers, low, high, mid, target_index)
+#         pygame.display.flip()
+#         if numbers[mid] == target:
+#             target_index = mid
+#             break
+#         elif numbers[mid] < target:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+#     draw_boxes(numbers, low, high, mid, target_index)
+#     pygame.display.flip()
+#     return target_index
 
 while running:
     for event in pygame.event.get():
